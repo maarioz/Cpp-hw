4c5b2f110ec51d937a932ad8451294168cf2ecb2
Mário Žák <33102086+maarioz@users.noreply.github.com>
2020-08-03 09:14:09 +0200

    Update reservation.h

--- 2018-01/reservation.h
+++ 2018-01/reservation.h
@@ -7,22 +7,22 @@
 class Reservation {
 
 public:
-	const std::string room_name;
-	const std::string course_code;
-	const int start_time;
-	const int end_time;
+    const std::string room_name;
+    const std::string course_code;
+    const int start_time;
+    const int end_time;
 
-	Reservation(const std::string &room_name, const std::string &course_code,
-	    int start_time, int end_time)
-	    : room_name(room_name)
-	    , course_code(course_code)
-	    , start_time(start_time)
-	    , end_time(end_time) {}
+    Reservation(const std::string &room_name, const std::string &course_code,
+        int start_time, int end_time)
+        : room_name(room_name)
+        , course_code(course_code)
+        , start_time(start_time)
+        , end_time(end_time) {}
 
-	void print() const {
-		std::cout << "Reservation for Course " << course_code << " in Room "
-		          << room_name << " [" << start_time << '-' << end_time << "]\n";
-	}
+   void print() const {
+        std::cout << "Reservation for Course " << course_code << " in Room " 
+             << room_name << " [" << start_time << '-' << end_time << "]\n";
+    }
 };
 
 #endif
