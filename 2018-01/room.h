6913fa8b8846aead440f32b84bdd1a19efb03e2c
Mário Žák <33102086+maarioz@users.noreply.github.com>
2020-08-03 09:10:27 +0200

    Update room.h

--- 2018-01/room.h
+++ 2018-01/room.h
@@ -8,58 +8,57 @@
 class Room {
 
 private:
-	std::array<bool, 24> room;
+    std::array<bool, 24> room;
 
 public:
-	const std::string name;
+    const std::string name;
 
-	Room(const std::string &name)
-	    : name(name) { room.fill(false); }
+    Room(const std::string &name) : name(name) { room.fill(false); }
 
-	bool isFree(int from, int to) const {
-		if (from < 0 || from > 24 || to <= 0 || to > 24 || from >= to) {
-			return false;
-		}
-		for (int i = from; i < to; i++) {
-			if (room[i]) {
-				return false;
-			}
-		}
-		return true;
+    bool isFree(int from, int to) const {
+	if (from < 0 || from > 24 || to <= 0 || to > 24 || from >= to) {
+	    return false;
 	}
-
-	int isFreeFor(int duration) const {
-		if (duration <= 0 || duration > 24) {
-			return -1;
-		}
-		for (int i = 0; i < static_cast<int>(room.size()); i++) {
-			if (!room[i] && isFree(i, duration + i)) {
-				return i;
-			}
-		}
-		return -1;
+	for (int i = from; i < to; i++) {
+	    if (room[i]) {
+	        return false;
+	    }
 	}
+	return true;
+    }
 
-	bool occupy(int from, int to) {
-		if (!isFree(from, to)) {
-			return false;
-		}
-		for (int i = from; i < to; i++) {
-			room[i] = true;
-		}
-		return true;
+    int isFreeFor(int duration) const {
+	if (duration <= 0 || duration > 24) {
+	    return -1;
 	}
+	for (int i = 0; i < static_cast<int>(room.size()); i++) {
+	    if (!room[i] && isFree(i, duration + i)) {
+		return i;
+	    }
+	}
+	return -1;
+    }
 
-	void print() const {
+    bool occupy(int from, int to) {
+	if (!isFree(from, to)) {
+	     return false;
+	}
+	for (int i = from; i < to; i++) {
+	    room[i] = true;
+	}
+	return true;
+    }
+
+    void print() const {
         std::cout << "Room: " + name + '\n';
     }
 
-	void schedule() const {
-		for (const bool &elem : room) {
-			std::cout << (elem ? 'X' : '_');
-		}
-		std::cout << '\n';
+    void schedule() const {
+        for (const bool &elem : room) {
+	    std::cout << (elem ? 'X' : '_');
 	}
+	std::cout << '\n';
+    }
 };
 
 #endif
